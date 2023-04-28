fcc9a4786f466c74f1bc3f6e8dc25b72499e24dd
Florian Muehleder <florian.muehleder@gmail.com>
2023-04-28 18:19:51 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {HeaderComponent} from "./header/header.component";
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShopingEditComponent } from './shopping-list/shoping-edit/shoping-edit.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent {
+recipes:Recipe[] =[
+  new Recipe('A Test Recipe', 'Simple Test', 'https://www.ich-liebe-brot.de/media/image/Baeckere-Huth-Rezept-Bruschetta.jpg'),
+  new Recipe('A Test Recipe', 'Simple Test', 'https://www.ich-liebe-brot.de/media/image/Baeckere-Huth-Rezept-Bruschetta.jpg')
+];
+}
